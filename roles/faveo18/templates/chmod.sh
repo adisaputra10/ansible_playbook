c26fcbac6ca9e6a7b68e5075142835463b284d46
Adi Saputra <adisaputra.id@gmail.com>
2021-07-24 19:59:42 +0700

    add helpdesk

---
+++ roles/faveo18/templates/chmod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+chown -R www-data:www-data /opt
+
+chown -R www-data:www-data /opt/faveo-helpdesk/
+
+chmod -R 755  /opt/
+
+chmod -R 755  /opt/faveo-helpdesk/
+
+chmod -R 755 /opt/faveo-helpdesk/storage
+
+chmod -R 755  /opt/faveo-helpdesk/bootstrap
